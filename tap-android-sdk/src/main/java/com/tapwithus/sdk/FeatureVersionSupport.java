958afc5d8402dbcbda29c3e664a55bda613fe491
KibiHofmann <kibi@tapwithus.com>
2022-11-08 13:01:25 +0200

    Fix up "supports" code to also have max values. So now the TapXR does NOT support mouse and Airmouse

--- tap-android-sdk/src/main/java/com/tapwithus/sdk/FeatureVersionSupport.java
+++ tap-android-sdk/src/main/java/com/tapwithus/sdk/FeatureVersionSupport.java
@@ -19,6 +19,11 @@ public class FeatureVersionSupport {
     public static final int FEATURE_DEVELOPER_MODE = 7;
     public static final int FEATURE_CONTROLLER_WITH_FULLHID = 8;
 
+    public static final int FIRST_TAP_XR_HW_VERSION = 40000; // as far as we know the TapXR will start at HW 4.0.0
+    public static final int FIRST_TAP_XR_FW_VERSION = 30000; // really no idea what version this is
+
+    public static final int MAX_VER_NUMBER = 99999;
+
     protected static Map<Integer, Integer> featureFwVer = new HashMap<Integer, Integer>() {{
         put(FEATURE_ENABLE_TEXT_MODE, 10000);
         put(FEATURE_MOUSE_MODE, 10500);
@@ -32,6 +37,17 @@ public class FeatureVersionSupport {
         put(FEATURE_CONTROLLER_WITH_FULLHID, 20405);
     }};
 
+    protected static Map<Integer, Integer> featureFwMaxVersion = new HashMap<Integer, Integer>() {{
+        put(FEATURE_ENABLE_TEXT_MODE, MAX_VER_NUMBER);
+        put(FEATURE_MOUSE_MODE, FIRST_TAP_XR_FW_VERSION - 1);
+        put(FEATURE_AIR_MOUSE, FIRST_TAP_XR_FW_VERSION - 1);
+        put(FEATURE_RAW_SENSOR, MAX_VER_NUMBER);
+        put(FEATURE_CONTROLLER_WITH_MOUSEHID, FIRST_TAP_XR_FW_VERSION - 1);
+        put(FEATURE_HAPTIC, MAX_VER_NUMBER);
+        put(FEATURE_DEVELOPER_MODE, MAX_VER_NUMBER);
+        put(FEATURE_CONTROLLER_WITH_FULLHID, FIRST_TAP_XR_FW_VERSION - 1);
+    }};
+
     protected static Map<Integer, Integer> featureHwVer = new HashMap<Integer, Integer>() {{
         put(FEATURE_ENABLE_TEXT_MODE, 30200);
         put(FEATURE_MOUSE_MODE, 30200);
@@ -41,6 +57,17 @@ public class FeatureVersionSupport {
         put(FEATURE_HAPTIC, 30200);
         put(FEATURE_DEVELOPER_MODE, 30200);
         put(FEATURE_CONTROLLER_WITH_FULLHID, 30200);
+    }};
+
+    protected static Map<Integer, Integer> featureHwMaxVersion = new HashMap<Integer, Integer>() {{
+        put(FEATURE_ENABLE_TEXT_MODE, MAX_VER_NUMBER);
+        put(FEATURE_MOUSE_MODE, FIRST_TAP_XR_HW_VERSION - 1);
+        put(FEATURE_AIR_MOUSE, FIRST_TAP_XR_HW_VERSION - 1);
+        put(FEATURE_RAW_SENSOR, MAX_VER_NUMBER);
+        put(FEATURE_CONTROLLER_WITH_MOUSEHID, FIRST_TAP_XR_HW_VERSION - 1);
+        put(FEATURE_HAPTIC, MAX_VER_NUMBER);
+        put(FEATURE_DEVELOPER_MODE, MAX_VER_NUMBER);
+        put(FEATURE_CONTROLLER_WITH_FULLHID, FIRST_TAP_XR_HW_VERSION - 1);
     }};
 
     public static boolean isFeatureSupported(@NonNull Tap tap, int feature) {
@@ -56,7 +83,21 @@ public class FeatureVersionSupport {
         if (featureFwVer.containsKey(feature)) {
             featureMinFwVer = featureFwVer.get(feature);
         }
-        return semVerToInt(hwVer) >= featureMinHwVer && semVerToInt(fwVer) >= featureMinFwVer;
+
+        // new part because we now have max values too
+        int featureMaxHwVer = 0;
+        if (featureHwMaxVersion.containsKey(feature)) {
+            featureMaxHwVer = featureHwMaxVersion.get(feature);
+        }
+
+        int featureMaxFwVer = 0;
+        if (featureFwMaxVersion.containsKey(feature)) {
+            featureMaxFwVer = featureFwMaxVersion.get(feature);
+        }
+
+//        return semVerToInt(hwVer) >= featureMinHwVer && semVerToInt(fwVer) >= featureMinFwVer;
+        return semVerToInt(hwVer) >= featureMinHwVer && semVerToInt(fwVer) >= featureMinFwVer &&
+                semVerToInt(hwVer) <= featureMaxHwVer && semVerToInt(fwVer) <= featureMaxFwVer;
     }
 
     public static int semVerToInt(String semVer) {
